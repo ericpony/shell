7927ddac7649b28b546e5bf1f6d9e695f851488d
ericpony <ericpony@csie.org>
2015-03-17 16:08:17 +0800

    refine

--- pull-gists.js
+++ pull-gists.js
@@ -1,9 +1,13 @@
-#! /usr/bin/node
+#!/usr/bin/env node
 
-var request = require('request'),
-    fs = require('fs'),
-    username = 'ericpony',
-    dir = './gists';  // save gists here
+var request  = require('request'), fs = require('fs');
+var username = process.argv[2];
+var dir      = process.argv[3] || './gists';  // save gists here
+
+if(!username) {
+  console.error('usage: ./' + process.argv[1].match(/[^\/\\]+$/)[0] + ' username (output-dir)');
+  process.exit(1);
+}
 
 var options = {
   url: 'https://api.github.com/users/' + username + '/gists',
@@ -17,20 +21,24 @@ request(options, function(error, response, body) {
   if (!error && response.statusCode == 200) {
     try {
       gists = JSON.parse(body);
-      gists.forEach(function (gist) {
-        console.log('Description: ', gist.description);
-        fs.mkdir(dir, function (err) {
+      gists.forEach(function(gist) {
+        fs.mkdir(dir, function(err) {
+          console.error('Gist url:    ', gist.url);
+          console.error('Description: ', gist.description || 'none');
+          console.error('Last update: ', gist.updated_at.match(/^\d+\-\d+\-\d\d/)[0]);
           for (var file in gist.files) {
             var url = gist.files[file].raw_url;
             var filename = gist.files[file].filename;
-            console.log('Downloading... ' + filename);
+            console.error('File:         ' + filename);
             request(url).pipe(fs.createWriteStream(dir + '/' + filename));
           }
+          console.error("------------");
         });
       });
     } catch(e) {
-      console.log('Exception: ' + e);
+      console.error('Exception: ' + e);
     }
-  } else
-      console.log('Error ' + response.statusCode);
+    return;
+  }
+  console.error('Error ' + response.statusCode);
 });
