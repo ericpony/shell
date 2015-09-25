3ddf73ba46333ba88f686271b62503f792ca3f55
ericpony <ericpony@csie.org>
2015-09-25 23:19:49 +0800

    avoid crontab timeout when the delay time is long

--- admin/auto-login.sh
+++ admin/auto-login.sh
@@ -6,8 +6,9 @@ LOGIN=
 
 URL="https://$USER:$PASS@$LOGIN"
 
-#sleep $(shuf -i 100-7200 -n 1) # random delay up to 2 hours
+DELAY=0
+#DELAY=$(shuf -i 100-7200 -n 1) # random delay up to 2 hours
 
 CREDENTIAL=$(curl -s $URL | sed -n 's/^.*name="\([^"]*\)" value="\([^"]*\)".*$/\1=\2/gp' | tr "\n" '&')
 
-curl -s $URL --data "$CREDENTIAL" >/dev/null
+echo "sleep $DELAY; curl -s '$URL' --data '$CREDENTIAL' >> /tmp/autologin.log" | bash &
